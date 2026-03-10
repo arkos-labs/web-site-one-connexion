ce986602e90fdf385416cf44e08958a7ca3bbef5
Developer <dev@local.host>
2026-03-10 21:02:47 +0100

    Fix: Add missing react-helmet-async dependency and wrap app in HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import { HelmetProvider } from 'react-helmet-async';
+
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
